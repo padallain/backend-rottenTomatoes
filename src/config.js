33a6786c2bfdc2ee80ea1def3a67cb3558056280
padallain <123842369+padallain@users.noreply.github.com>
2024-11-08 22:23:18 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,7 +6,6 @@ export const DB_URI = process.env.DB_URI;
 export const PORT = process.env.PORT || 8000;
 export const SECRET_KEY = process.env.SECRET_KEY;
 
-const mongoose = require('mongoose');
 
  export const connectToDatabase = async () => {
     try {
