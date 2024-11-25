c9acb21ccd5a2a8e84f673c32d73291dbd39c3eb
padallain <123842369+padallain@users.noreply.github.com>
2024-11-25 00:53:30 -0400

    add seriesModel

---
+++ src/models/series.model.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+
+const seriesSchema = new mongoose.Schema({
+  seriesId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  banner: {
+    type: String
+  },
+  caratula: {
+    type: String
+  },
+  duration: {
+    type: Number
+  },
+  briefDescription: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  releaseDate: {
+    type: Date
+  },
+  genre: {
+    type: [String]
+  },
+  director: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  cast: [{
+    id: Number,
+    name: String,
+    role: String,
+    profile_path: String,
+    character: String
+  }],
+  ratings: {
+    type: Number
+  },
+  categories: {
+    type: [String]
+  },
+  budget: {
+    type: Number
+  },
+  originalLanguage: {
+    type: String
+  }
+}, {
+  timestamps: true
+});
+
+const SeriesModel = mongoose.model('Series', seriesSchema);
+
+export default SeriesModel;
