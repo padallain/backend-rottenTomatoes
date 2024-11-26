cf221b5e93d94087792ab2ae1f2f0dcdad39039a
padallain <123842369+padallain@users.noreply.github.com>
2024-11-26 11:00:05 -0400

    some new changes

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -228,11 +228,15 @@ async removeFromWatchlist(req, res) {
     }
   }
 
+  a  // Get user's last seen movies with full movie details
   async getLastSeen(req, res) {
-    const { userId } = req.body; // Assuming userId is passed in the request body
+    const { personId } = req.params;
 
     try {
-      const user = await User.findById(userId).populate("lastSeenMovies");
+      const user = await User.findById(personId).populate({
+        path: 'lastSeenMovies.movie',
+        model: 'Movie'
+      });
 
       if (!user) {
         return res.status(404).json({ message: "User not found" });
@@ -241,12 +245,7 @@ async removeFromWatchlist(req, res) {
       res.status(200).json({ lastSeenMovies: user.lastSeenMovies });
     } catch (error) {
       console.error("Error fetching last seen movies:", error);
-      res
-        .status(500)
-        .json({
-          message: "Error fetching last seen movies",
-          error: error.message,
-        });
+      res.status(500).json({ message: "Error fetching last seen movies", error: error.message });
     }
   }
 
