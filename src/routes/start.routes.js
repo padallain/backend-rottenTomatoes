5469bd49065d4b60c6b71776178a2499a2a9a4eb
padallain <123842369+padallain@users.noreply.github.com>
2024-11-30 07:58:01 -0400

    fix previous commit

--- src/routes/start.routes.js
+++ src/routes/start.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, createLogin, resetPassword, checkResetToken, savePassword, eraseAccount } from '../controllers/auth.controllers.js';
+import { register, createLogin, resetPassword, checkResetToken, savePassword, eraseAccount,getUserInfoById } from '../controllers/auth.controllers.js';
 import Movie from '../controllers/movie.controllers.js'; 
 import Series from '../controllers/series.controllers.js';
 import ReviewController from '../controllers/review.controllers.js';
